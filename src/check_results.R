4555f22d3ceccb1e0b8c6f2c9c07d27a7d683add
hanadate <s2421310@u.tsukuba.ac.jp>
2024-11-11 00:14:14 +0900

    add backtest

--- src/check_results.R
+++ src/check_results.R
@@ -12,7 +12,7 @@ library(doParallel)
 library(pROC)
 
 #===== settings
-pred_days <- c(10,20)
+pred_days <- c(5,10,15)
 i <- pred_days[2]
 sectors <- c(
   # 9 Basic Sectors
@@ -28,6 +28,11 @@ winner_x <- read_csv(paste0("doc/winner_x_", i,".csv")) %>%
   dplyr::mutate_at(c("month_of_year", "day_of_week",
                      "nth_wday_month", "month_of_year",
                      "day_of_week","week_of_month"),as.factor)
+# trained model
+modelFit <- readRDS(paste0("doc/modelFit_", i,".rds"))
+# prices
+prices_ohlc <- readRDS("doc/sectors_prices.rds")
+adjusted_prices <- lapply(prices_ohlc, function(x){(Cl(adjustOHLC(x,use.Adjusted=TRUE)))})
 
 #===== check best hyper params
 modelFit$bestTune
@@ -52,3 +57,98 @@ summary(winner_x_pred)
 # find duplicated date
 length(winner_x$actual_date)==
   length(unique(winner_x$actual_date))
+
+#===== Easy Backtest
+# Note: This backtest is rough and optimistic. 
+#       Move window for date of train data every oneday to perform legit backtest.
+#===
+# Check exist predictions
+predictions <- paste0("doc/",dir("doc")[str_starts(dir("doc"),"pred_prob_dump")])
+
+# セクターごとのOutperform確率
+today_rate <- read_csv(predictions[2], col_select=-1)
+invest_rate <- today_rate %>%
+  dplyr::select(-actual_date) %>% 
+  # map predicted probability to ticker
+  dplyr::mutate(PRED = names(.)[max.col(.)]) %>% 
+  dplyr::mutate(actual_date=today_rate$actual_date) %>%
+  # if inv has highest prob, set position 0.
+  dplyr::mutate(across(all_of(sectors), ~if_else(PRED=="inv", 0, .)))
+invest_rate %>% glimpse
+invest_rate %>% tail
+invest_rate %>% summary
+
+# ETF価格
+adjusted_prices_df <- as.data.frame(do.call(cbind, adjusted_prices)) %>%
+  dplyr::mutate(actual_date=lubridate::ymd(row.names(.)))
+# ETF価格とOutperform確率
+invest_rate_prices <- invest_rate %>%
+  dplyr::inner_join(., adjusted_prices_df, by="actual_date")
+# ETF価格と投資配分をactual_dateでjoin
+# セクターごとのdata.frameにして，ひとつのListにまとめる
+ticker_c <- sectors
+invest_rate_prices_list <- foreach(i=1:length(ticker_c), .combine="rbind") %do% {
+  target_ticker <- ticker_c[i]
+  target_invest_rate_prices <- invest_rate_prices %>%
+    dplyr::select(actual_date, starts_with(target_ticker)) %>%
+    dplyr::rename(invest_rate := !!target_ticker,
+                  price := !!paste0(target_ticker, ".Close")) %>%
+    dplyr::mutate(ticker=target_ticker)
+}
+leverage_invest_rate_prices_list <- invest_rate_prices_list %>%
+  dplyr::mutate(invest_rate=case_when(
+    ticker %in% c("XLE") ~ invest_rate*2,
+    ticker %in% c("XLF","XLK","XLV") ~ invest_rate*3,
+    .default = invest_rate
+  ))
+# 本戦略のリターン
+# レバレッジの場合は、
+# エネ ERX = XLE x2
+# 金融 FAS = XLF x3
+# 技術 TECL = XLK x3
+# 長期債 TMF = TLT x3
+# ヘルス CURE = XLV x3
+leverage_on_c <- c(FALSE, TRUE)
+comp_ticker_c <- c("SPY", "SPXL")
+comp_duration_c <- c(20,200,1000,2000)
+
+for(i in seq(length(leverage_on_c))) {for(j in seq(length(comp_duration_c))) {
+  leverage_on <- leverage_on_c[i]
+  comp_ticker <- comp_ticker_c[i]
+  comp_duration <- comp_duration_c[j]
+  print(paste0("lev:",leverage_on,", comp:",comp_ticker, " dur:",comp_duration))
+
+  performance_daily_return <- foreach(k=seq(length(ticker_c)), .combine = "+") %do% {
+    target_ticker <- ticker_c[k]
+    target_df <- if(leverage_on==TRUE){leverage_invest_rate_prices_list}else{invest_rate_prices_list}
+    target_invest_rate_prices <- target_df %>%
+      dplyr::filter(ticker %in% target_ticker) %>%
+      dplyr::select(actual_date, price) %>%
+      as.xts %>%
+      dailyReturn(., type="arithmetic")
+    target_rate <- target_df %>%
+      dplyr::filter(ticker %in% target_ticker) %>%
+      dplyr::select(actual_date, invest_rate) %>%
+      as.xts
+    target_daily_return <- target_invest_rate_prices*target_rate
+  } %>%
+    setNames(., paste0("mystrategy","_lev_",leverage_on))
+  # PerformanceAnalytics::charts.PerformanceSummary(performance_daily_return)
+
+  comp_value_list <- new.env()
+  getSymbols(comp_ticker, from = "2006-03-01", to = today(),
+             env = comp_value_list,  src = "yahoo", warnings = FALSE)
+  comp_value <- as.list(comp_value_list)[[1]]
+
+  SPY_dailyreturn <- comp_value %>%
+    adjustOHLC(., use.Adjusted = TRUE) %>%
+    dailyReturn(., type="arithmetic") %>%
+    setNames(., comp_ticker)
+  mystrategy_spy <- merge.xts(performance_daily_return, SPY_dailyreturn)
+  png(filename=paste0("doc/vs",comp_ticker,"_",comp_duration,"_lev_",leverage_on,".png"),width = 480, height = 480)
+  # for loop内ではprintしないといけない
+  print(chart.CumReturns(tail(mystrategy_spy,comp_duration), legend.loc = "bottomright", wealth.index = TRUE))
+  dev.off()
+}}
+#========
+
--- src/sector_analytics.R
+++ src/sector_analytics.R
@@ -5,6 +5,7 @@
 # Take advantage of Interpolation, not Extrapolation.
 # Jim Cramer frequently said THERE'S ALWAYS A BULL MARKET SOMEWHERE.
 # Short is risker than long. https://www.investopedia.com/terms/s/shortselling.asp
+# Add inverse return of GSPC into target classes to catch a DON'T LONG signal.
 #===== Libraries
 library(tidyverse)
 library(lubridate)
@@ -425,6 +426,7 @@ Ene  XLE x2= ERX
 Heal XLV x3= CURE
 Util XLU
 Stap XLP
+-SP5 inv
 # Note: Sell fast Leveraged ETFs. It must decay in a long term.
 ", file=result_file, append=TRUE)
 write_lines(paste0("START: ",round(starttime)), file=result_file, append=TRUE)
@@ -445,102 +447,3 @@ write_lines(paste0(names(trControl)," : " , trControl), file=result_file, append
 write_lines("===tuneGrid===", file=result_file, append=TRUE)
 write_lines(paste0(names(tuneGrid)," : " , tuneGrid), file=result_file, append=TRUE)
 print("PREDICT FINISHED.")
-
-#=====Backtest
-#=======
-# print("Start Backtest")
-# print(now())
-# # Check exist predictions
-# predictions <- paste0("doc/",dir("doc")[str_starts(dir("doc"),"pred_prob_dump")])
-# 
-# # セクターごとのOutperform確率
-# today_rate <- read_csv(predictions[1])
-# invest_rate <- today_rate %>%
-#   dplyr::select(-actual_date) %>%
-#   # positive threshold +.3= >=.2でpositive
-#   +.3 %>%
-#   round(0) %>%
-#   dplyr::mutate(actual_date=today_rate$actual_date) %>%
-#   # 合計値で配分比
-#   dplyr::mutate(tot=XLK+XLF+XLI+XLB+XLY+XLE+XLV+XLU+XLP+TLT) %>%
-#   # 合計値で割る
-#   dplyr::mutate(across(c(XLK,XLF,XLI,XLB,XLY,XLE,XLV,XLU,XLP,TLT), ~.x/tot)) %>%
-#   # nanを0で補完
-#   dplyr::mutate(across(everything(), ~replace_na(.x,0)))
-# invest_rate %>% tail
-# invest_rate %>% summary
-# 
-# # ETF価格
-# adjusted_prices_df <- as.data.frame(do.call(cbind, adjusted_prices)) %>%
-#   dplyr::mutate(actual_date=lubridate::ymd(row.names(.)))
-# # ETF価格とOutperform確率
-# invest_rate_prices <- invest_rate %>%
-#   dplyr::inner_join(., adjusted_prices_df, by="actual_date")
-# # ETF価格と投資配分をactual_dateでjoin
-# # セクターごとのdata.frameにして，ひとつのListにまとめる
-# ticker_c <- c("XLK","XLF","XLB","XLI","XLY","XLE","XLV","XLP","XLU","TLT")
-# invest_rate_prices_list <- foreach(i=1:length(ticker_c), .combine="rbind") %do% {
-#   target_ticker <- ticker_c[i]
-#   target_invest_rate_prices <- invest_rate_prices %>%
-#     dplyr::select(actual_date, starts_with(target_ticker)) %>%
-#     dplyr::rename(invest_rate := !!target_ticker,
-#                   price := !!paste0(target_ticker, ".Close")) %>%
-#     dplyr::mutate(ticker=target_ticker)
-# }
-# leverage_invest_rate_prices_list <- invest_rate_prices_list %>%
-#   dplyr::mutate(invest_rate=case_when(
-#     ticker %in% c("XLE") ~ invest_rate*2,
-#     ticker %in% c("XLF","XLK","XLV","TLT") ~ invest_rate*3,
-#     .default = invest_rate
-#   ))
-# # 本戦略のリターン
-# # レバレッジの場合は、
-# # エネ ERX = XLE x2
-# # 金融 FAS = XLF x3
-# # 技術 TECL = XLK x3
-# # 長期債 TMF = TLT x3
-# # ヘルス CURE = XLV x3
-# leverage_on_c <- c(FALSE, TRUE)
-# comp_ticker_c <- c("SPY", "SPXL")
-# comp_duration_c <- c(20,200,1000,2000)
-# 
-# for(i in seq(length(leverage_on_c))) {for(j in seq(length(comp_duration_c))) {
-#   leverage_on <- leverage_on_c[i]
-#   comp_ticker <- comp_ticker_c[i]
-#   comp_duration <- comp_duration_c[j]
-#   print(paste0("lev:",leverage_on,", comp:",comp_ticker, " dur:",comp_duration))
-# 
-#   performance_daily_return <- foreach(k=seq(length(ticker_c)), .combine = "+") %do% {
-#     target_ticker <- ticker_c[k]
-#     target_df <- if(leverage_on==TRUE){leverage_invest_rate_prices_list}else{invest_rate_prices_list}
-#     target_invest_rate_prices <- target_df %>%
-#       dplyr::filter(ticker %in% target_ticker) %>%
-#       dplyr::select(actual_date, price) %>%
-#       as.xts %>%
-#       dailyReturn(., type="arithmetic")
-#     target_rate <- target_df %>%
-#       dplyr::filter(ticker %in% target_ticker) %>%
-#       dplyr::select(actual_date, invest_rate) %>%
-#       as.xts
-#     target_daily_return <- target_invest_rate_prices*target_rate
-#   } %>%
-#     setNames(., paste0("mystrategy","_lev_",leverage_on))
-#   # PerformanceAnalytics::charts.PerformanceSummary(performance_daily_return)
-# 
-#   comp_value_list <- new.env()
-#   getSymbols(comp_ticker, from = "2006-03-01", to = today(),
-#              env = comp_value_list,  src = "yahoo", warnings = FALSE)
-#   comp_value <- as.list(comp_value_list)[[1]]
-# 
-#   SPY_dailyreturn <- comp_value %>%
-#     adjustOHLC(., use.Adjusted = TRUE) %>%
-#     dailyReturn(., type="arithmetic") %>%
-#     setNames(., comp_ticker)
-#   mystrategy_spy <- merge.xts(performance_daily_return, SPY_dailyreturn)
-#   png(filename=paste0("doc/vs",comp_ticker,"_",comp_duration,"_lev_",leverage_on,".png"),width = 480, height = 480)
-#   # for loop内ではprintしないといけない
-#   print(chart.CumReturns(tail(mystrategy_spy,comp_duration), legend.loc = "bottomright", wealth.index = TRUE))
-#   dev.off()
-# }}
-# #========
-# 
